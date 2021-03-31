afac7ea266e400e7f8dfc79e0f633327dd94fc34
ElmerCubasSilva <81631874+ElmerCubasSilva@users.noreply.github.com>
2021-03-31 13:44:58 -0500

    CajeroAutomatico

---
+++ App.java
@@ -0,0 +1,239 @@
+package com.senati.eti;
+import com.senati.eti.modelo.Usuario;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+public class App {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		
+		//Definiendo usuarios
+		ArrayList<Usuario> usuarios=new ArrayList<Usuario>();
+		usuarios.add(new Usuario(98765,"123a",506.3f,"Pedro"));
+		usuarios.add(new Usuario(12345, "123b",600.0f,"Maria"));
+		
+		
+		
+		while(true) {
+			Usuario usuario_actual = null;
+			boolean reiniciar_ejecucion=false;
+		
+			System.out.print("Ingrese el número de tarjeta : ");
+			int num_tarjeta=sc.nextInt();
+			int indice_usuario=verificar_tarjeta(num_tarjeta,usuarios);
+			if(indice_usuario==-1) {
+				System.out.println("------------------");
+				System.out.println("¡Tarjeta invalida!");
+				System.out.println("------------------");
+				System.out.println("");
+				reiniciar_ejecucion=true;
+			} else {
+				usuario_actual=usuarios.get(indice_usuario);
+			}
+			
+			if(reiniciar_ejecucion)
+				continue;
+			
+			int intentos=1;
+			do {
+				System.out.print("Ingrese la contraseña : ");
+				String contrasena=sc.next();
+				if(contrasena.equals(usuario_actual.getContrasena())) {
+					intentos=1;
+					break;
+				}else if(intentos==3) {
+					reiniciar_ejecucion=true;
+					System.out.println("-------------------------");
+					System.out.println("¡Fallaste los 3 intentos!");
+					System.out.println("-------------------------");
+					System.out.println("");
+					break;
+				} else {
+					intentos++;
+					System.out.println("---------------------");
+					System.out.println("¡Contraseña invalida!");
+					System.out.println("Intentos restantes : "+(4-intentos));
+					System.out.println("---------------------");
+				}
+			} while(true);
+			
+			if(reiniciar_ejecucion)
+				continue;
+			
+			//opciones
+			while(true) {
+				boolean terminar_opciones=false;
+				System.out.println("");
+				System.out.println("OPERACIONES");
+				System.out.println("1. Consultar saldo");
+				System.out.println("2. Deposito");
+				System.out.println("3. Retiro");
+				System.out.println("4. Transferencia");
+				System.out.println("5. Salir");
+				System.out.print("Que operacion quieres realizar : ");
+				int op=sc.nextInt();
+				System.out.println("");
+				
+				op=(op<0 && op>3)?-1:op;
+				
+				boolean ejecucion=ejecutar_opciones(op,usuario_actual,sc,usuarios);
+				if(ejecucion) {
+					System.out.println("-----------------------------");
+					System.out.println("Operacion realizada con exito");
+					System.out.println("-----------------------------");
+					System.out.println("");
+					System.out.println("¿Quieres realizar otra opcion ?  ");
+					
+					while(true) {
+						
+						System.out.println("1. Si");
+						System.out.println("2. No");
+						System.out.print("Elegir : ");
+						int op_reinicio=sc.nextInt();
+						if(op_reinicio==2) {
+							System.out.println("-----------------");
+							System.out.println("Sesion finalizada");
+							System.out.println("-----------------");
+							terminar_opciones=true;
+							break;
+						} else if(op_reinicio!=1) {
+							System.out.println("Opcion invalida, elija una opcion ");
+						} else {
+							break;
+						}
+					} 
+					
+				} else if(!ejecucion && op==2) { // todos los else siguientes es para manejo de errores
+					System.out.println("");
+					System.out.println("Error : la cantidad debe ser mayor igual a 10");
+				}else if(!ejecucion && op==3) {
+					System.out.println("");
+					System.out.println("Error : Verificar la cantidad");
+					System.out.println("Debe ser mayor de 10 y menor al saldo disponible");
+					System.out.println("Billetes disponibles : 10,20,50,100");
+				} else if(!ejecucion && op==4) {
+//					System.out.println("Error : Verificar la cantidad");
+//					System.out.println("Debe ser mayor de 10 y menor al saldo disponible");
+				}  else if(!ejecucion && op==5) {
+					System.out.println("-----------------");
+					System.out.println("Sesion finalizada");
+					System.out.println("-----------------");
+					break;
+				} else {
+					System.out.println("----------------");
+					System.out.println("Opcion no valida");
+					System.out.println("----------------");
+				}
+				if(terminar_opciones)
+					break;
+			} 
+		} 
+	}
+	
+	public static int verificar_tarjeta(int tarjeta,ArrayList<Usuario> usuarios) {
+		int indice_usuario=0;
+		for(int x=0;x<usuarios.size();x++) {
+			if(tarjeta==usuarios.get(x).getTarjeta()) {
+				indice_usuario=x;
+				break;
+			} else if(x==usuarios.size()-1) {
+				indice_usuario=-1;
+				break;
+			}
+		}
+		return indice_usuario;
+		
+	}
+
+	public static boolean ejecutar_opciones(int op,Usuario user,Scanner sc,ArrayList<Usuario> usuarios) {
+		String par[];
+		
+		switch (op) {
+		case 1:
+			par=new String[] {"Operacion","Consulta","Saldo ",""+user.getSaldo()};
+			vaucher(user,par);
+			break;
+		case 2:
+			System.out.print("Ingrese la cantidad : ");
+			float cantidad=sc.nextFloat();
+			if(cantidad>=10) {
+				user.depositar(cantidad);
+				par=new String[] {"Operacion","Deposito","Monto",cantidad+"","Saldo",user.getSaldo()+""};
+				vaucher(user,par);
+			} else
+				return false;
+			
+			break;
+		case 3:
+			float retiro;
+
+			System.out.println("Billetes disponibles : 10,20,50,100 ");
+			System.out.print("Cantidad a retirar : ");
+			retiro=sc.nextFloat();
+			if(user.retirar(retiro) && retiro>=10) {
+				par=new String[] {"Operacion","Retiro","Monto",""+retiro,"Saldo",""+user.getSaldo()};
+				vaucher(user,par);
+				break;
+			} else {
+				return false;
+			}
+			
+		case 4:
+			System.out.print("Cuenta a depositar : ");
+			int cuenta=sc.nextInt();
+			int id=verificar_tarjeta(cuenta,usuarios);
+			if(id==-1) {
+				System.out.println("");
+				System.out.println("Error : La cuenta ingresada no existe");
+				return false;
+			}else if(user.getTarjeta()==usuarios.get(id).getTarjeta()) {
+				System.out.println("");
+				System.out.print("Error : No se puede transferir a la misma cuenta");
+				System.out.print("");
+				return false;
+			} else {
+				System.out.print("Ingrese la cantidad : ");
+				float cant=sc.nextFloat();
+				if(user.transferir(cant) && cant>10){
+					Usuario usuario_transf=usuarios.get(id);
+					usuario_transf.depositar(cant);
+					par=new String[] {"Operacion","Transferencia","Cuenta destino",usuario_transf.getTarjeta()+"",
+							"Monto",cant+"","Saldo",user.getSaldo()+""};
+					vaucher(user,par);
+					return true;
+				} else {
+					System.out.println("");
+					System.out.println("Error : verifique la cantidad");
+					System.out.println("Debe ser mayor a 10 y menor a tu saldo actual");
+					return false;
+				}
+			}	
+		case 5:
+			return false;
+		default:
+			return false;
+		}
+		return true;
+	}
+	public static void vaucher(Usuario usuario,String[] pares) {
+		String patron="%-12s : %-20s";
+		Date date=new Date();
+		DateFormat formato_fecha = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		System.out.println("");
+		System.out.println("*************");
+		System.out.println("---VAUCHER---");
+		System.out.println("*************");
+		System.out.println(String.format(patron,"Usuario",usuario.getNombre()));
+		System.out.println(String.format(patron,"Fecha y hora",formato_fecha.format(date)));
+		for(int i=0;i<pares.length;) {
+			System.out.println(String.format(patron,pares[i],pares[i+1]));
+			i+=2;
+		}
+		System.out.println("");
+		
+	}
+}
---
+++ Usuario.java
@@ -0,0 +1,90 @@
+package com.senati.eti.modelo;
+
+import java.text.DecimalFormat;
+
+public class Usuario {
+	private String nombre;
+	private int tarjeta;
+	private float saldo;
+	private String contrasena;
+	DecimalFormat formateador = new DecimalFormat("####.##");
+
+	public float getSaldo() {
+		return this.saldo;
+	}
+
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getTarjeta() {
+		return this.tarjeta;
+	}
+
+	public void setTarjeta(int tarjt) {
+		this.tarjeta = tarjt;
+	}
+
+	public String getContrasena() {
+		return this.contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+
+	// Constructor
+	public Usuario(int numero, String contrasena, float saldo, String nombre) {
+		setTarjeta(numero);
+		setContrasena(contrasena);
+		setSaldo(saldo);
+		setNombre(nombre);
+
+	}
+
+	// Metodos de usuario
+
+	public boolean retirar(float cantidad_retirada) {
+		if(verificar_retiro(cantidad_retirada) && verificar_billetes(cantidad_retirada)) {
+			this.saldo = Float.parseFloat(formateador.format(this.saldo-cantidad_retirada));
+			return true;
+		}else {
+			return false;
+		}
+			
+		
+	}
+
+	public boolean transferir( float cantidad) {
+		if(verificar_retiro(cantidad) ) {
+			this.saldo =Float.parseFloat(formateador.format(this.saldo-cantidad)) ;
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+	public void depositar(float cantidad) {
+		this.saldo += cantidad;
+	}
+	public boolean verificar_retiro(float cantidad) {
+		if(cantidad>this.saldo || cantidad<0)
+			return false;
+		else
+			return true;
+	}
+	public boolean verificar_billetes(float cantidad) {
+		if(cantidad%10==0)
+			return true;
+		else
+			return false;
+	}
+}
